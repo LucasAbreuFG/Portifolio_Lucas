504303a2a119c369cf8c983a0190fe5585aa01b4
LucasAbreuFG <lucasdeafg@yahoo.com.br>
2024-10-21 10:40:23 -0300

    feat_contato

--- src/components/ContatoComponent.js
+++ src/components/ContatoComponent.js
@@ -1,28 +1,35 @@
 
 import React from 'react';
 import '../App.css'
+import '../styles/ContatoStyle.css'
 
+const whatsappNumber = process.env.REACT_APP_WHATSAPP_NUMBER;
+const linkedin = process.env.REACT_APP_LINKEDIN;
+const github = process.env.REACT_APP_GITHUB;
 const Contato = () => {
-    return (
+  return (
     <section id="contato">
-          <h2>Vamos Trabalhar Juntos</h2>
-          <div class="terminal">
-            <div class="terminal-content">
-              <p class="command">echo $CONTACT_INFO</p>
-              <p class="response">
-                <p>Email: lucasTeste@gmail.cm</p>
-                <p>LinkedIn: linkedin.com/in/lucas-abreu-garcia/</p>
-                <p>GitHub: github.com/LucasAbreuFG</p>
-              </p>
-              <p class="command">ssh lucas@collaborate</p>
-              <p class="response">
-                Estou sempre aberto a novas oportunidades e parcerias desafiadoras. Se você está buscando um desenvolvedor apaixonado por criar soluções inovadoras e impactantes, adoraria discutir como posso contribuir para o sucesso do seu projeto. Vamos transformar ideias em realidade!
-              </p>
-            </div>
-          </div>
-          <a href="mailto:joao.silva@example.com" class="cta-button">Iniciar um Projeto Inovador</a>
-        </section>
-    )
+      <h2>Vamos Trabalhar Juntos</h2>
+      <div class="terminal">
+        <div class="terminal-content">
+          <p class="command">echo $CONTACT_INFO</p>
+          <p class="response">
+            <a href={`${linkedin}`} target="_blank" rel="noopener noreferrer" className="contato-link">
+              <p>LinkedIn: linkedin.com/in/lucas-abreu-garcia/</p>
+            </a>
+            <a href={`${github}`}target="_blank" rel="noopener noreferrer" className="contato-link">
+            <p>GitHub: github.com/LucasAbreuFG</p>
+            </a>
+          </p>
+          <p class="command">ssh lucas@collaborate</p>
+          <p class="response">
+            Estou sempre aberto a novas oportunidades e parcerias desafiadoras. Se você está buscando um desenvolvedor apaixonado por criar soluções inovadoras e impactantes, adoraria discutir como posso contribuir para o sucesso do seu projeto. Vamos transformar ideias em realidade!
+          </p>
+        </div>
+      </div>
+      <a href={`https://wa.me/${whatsappNumber}`} class="cta-button">Iniciar um Projeto Inovador</a>
+    </section>
+  )
 }
 
 export default Contato
--- src/components/DepoimentosComponent.js
+++ src/components/DepoimentosComponent.js
@@ -6,14 +6,35 @@ const Depoimentos = () => {
     return (
         <section id="depoimentos">
             <h2>Depoimentos</h2>
-            <div class="testimonial">
-                <p class="testimonial-content">"João é um desenvolvedor excepcional. Sua habilidade técnica, criatividade e dedicação ao projeto superaram todas as nossas expectativas. Ele não apenas entregou um produto de alta qualidade, mas também trouxe ideias inovadoras que elevaram nosso projeto a um novo patamar."</p>
-                <p class="testimonial-author">- Maria Santos, CEO da TechInova</p>
-            </div>
-            <div class="testimonial">
-                <p class="testimonial-content">"Trabalhar com João foi uma experiência transformadora para nossa equipe. Seu conhecimento técnico profundo, combinado com sua habilidade de comunicação e liderança, impulsionou significativamente a produtividade e a qualidade de nossos projetos. João não é apenas um desenvolvedor talentoso, mas um verdadeiro catalisador de inovação."</p>
-                <p class="testimonial-author">- Carlos Oliveira, CTO da StartUp Plus</p>
-            </div>
+            <a href="https://www.linkedin.com/in/lucas-abreu-garcia/details/recommendations/?detailScreenTabIndex=0" target="_blank" rel="noopener noreferrer" className="testimonial-link">
+                <div class="testimonial">
+                    <p class="testimonial-content">"Lucas se destaca por sua dedicação e capacidade de aprender rapidamente, sempre trazendo soluções técnicas robustas para os desafios que enfrentamos. Seu compromisso com a excelência e habilidade para lidar com situações complexas contribuíram significativamente para o sucesso dos nossos projetos.
+                        <br />Além das competências técnicas, Lucas possui uma excelente comunicação, sempre mantendo uma interação respeitosa e produtiva com clientes e colegas..."</p>
+                    <p class="testimonial-author">- Rodrigo Mello, Senior Fullstack Developer da Kruzer</p>
+                </div>
+            </a>
+            <a href="https://www.linkedin.com/in/lucas-abreu-garcia/details/recommendations/?detailScreenTabIndex=0" target="_blank" rel="noopener noreferrer" className="testimonial-link">
+                <div class="testimonial">
+                    <p class="testimonial-content">"Lucas é uma pessoa sensacional para trabalhar junto. Muito dedicado, capacitado e de ótima convivência. Sempre atento ao que vai gerar valor para a empresa e a equipe."</p>
+                    <p class="testimonial-author">- Arthur Rodrigues, Software Enginer da Semantix</p>
+                </div>
+            </a>
+            <a href="https://www.linkedin.com/in/lucas-abreu-garcia/details/recommendations/?detailScreenTabIndex=0" target="_blank" rel="noopener noreferrer" className="testimonial-link">
+                <div class="testimonial">
+                    <p class="testimonial-content">"Lucas é um profissional muito orientado a resultados, tive a oportunidade de trabalhar lado a lado com ele em grandes projetos de tecnologia. Possuí uma comunicação técnica excelente e domina hard skills de desenvolvimento de software.
+                        <br />Lucas pode e vai atender as necessidades de qualquer empresa."</p>
+                    <p class="testimonial-author">- Matheus Marques, Squard Leader da Semantix</p>
+                </div>
+            </a>
+            <a href="https://www.linkedin.com/in/lucas-abreu-garcia/details/recommendations/?detailScreenTabIndex=0" target="_blank" rel="noopener noreferrer" className="testimonial-link">
+                <div class="testimonial">
+                    <p class="testimonial-content">"Tive a oportunidade de trabalhar com o Lucas por 8 meses, dos quais 6 fui seu tech lead.
+                        O Lucas é um profissional dedicado, que sempre está em busca de novos desafios.
+                        Nos projetos que atuamos, ele esteve também a frente da comunicação com o cliente, sempre sendo cordial e respeitoso com todos.
+                        Tecnicamente o Lucas sempre trazia ótimas soluções, sempre respeitando os demais colegas e ouvindo as opiniões de todos."</p>
+                    <p class="testimonial-author">- Cezar Menzzalira, Software Developer Specialist da Semantix</p>
+                </div>
+            </a>
         </section>
 
     )
