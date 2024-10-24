3a450d4a7089ab22e9e7eaf060a0dbed7e7de7cd
LucasAbreuFG <lucasdeafg@yahoo.com.br>
2024-10-24 14:11:52 -0300

    fix

--- src/components/HabilidadesComponent.js
+++ src/components/HabilidadesComponent.js
@@ -7,30 +7,13 @@ const Habilidades = () => {
 <section id="habilidades">
           <h2>Habilidades Técnicas</h2>
           <div class="skills">
-            <span class="skill">JavaScript</span>
-            <span class="skill">TypeScript</span>
-            <span class="skill">Python</span>
-            <span class="skill">React</span>
-            <span class="skill">Vue.js</span>
-            <span class="skill">Angular</span>
-            <span class="skill">Node.js</span>
-            <span class="skill">Express</span>
-            <span class="skill">Django</span>
-            <span class="skill">Flask</span>
-            <span class="skill">React Native</span>
-            <span class="skill">GraphQL</span>
-            <span class="skill">REST APIs</span>
-            <span class="skill">SQL</span>
-            <span class="skill">MongoDB</span>
-            <span class="skill">Redis</span>
-            <span class="skill">Git</span>
-            <span class="skill">Docker</span>
-            <span class="skill">Kubernetes</span>
-            <span class="skill">AWS</span>
-            <span class="skill">Azure</span>
-            <span class="skill">CI/CD</span>
-            <span class="skill">TDD</span>
-            <span class="skill">Agile/Scrum</span>
+            <span class="skill">Desenvolvimento de plataformas</span>
+            <span class="skill">Desenvolvimento de APIs</span>
+            <span class="skill">Criação de Aplicativos</span>
+            <span class="skill">Integração de Sistemas</span>
+            <span class="skill">Banco de Dados</span>
+            <span class="skill">Metodologias Ágeis</span>
+            <span class="skill">Cloud Computing</span>
           </div>
         </section>
     )
