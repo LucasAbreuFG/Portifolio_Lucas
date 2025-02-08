3d14b77392f98d08cad1b8d10409dc9093f9bece
Lucas de Abreu Furtado Garcia <52716819+LucasAbreuFG@users.noreply.github.com>
2025-02-08 09:48:12 -0300

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -12,7 +12,7 @@ const Header = () => {
             <p className="subtitle">Desenvolvedor Full Stack Inovador</p>
           </div>
           <div class="header-image">
-            <img src="https://media.licdn.com/dms/image/v2/D4D03AQHeKv0vMWM4dw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1694120366253?e=1734566400&v=beta&t=ZgylQEAt2bA1P228MqfxxNrIv0nnauk6uRLyhi8JYCA" alt="Lucas de Abreu - Desenvolvedor Full Stack" width="250" height="250" />
+            <img src="https://avatars.githubusercontent.com/u/52716819?s=400&u=ce71d2419252819b4815c54d975f8a3805e66354&v=4" alt="Lucas de Abreu - Desenvolvedor Full Stack" width="250" height="250" />
           </div>
         </div>
       </div>
